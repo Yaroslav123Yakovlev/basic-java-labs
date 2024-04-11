5580575db4f52c98f5a489f0be4b4b4e2aa8bf24
Yaroslav123Yakovlev <yakovliev.y.y@nmu.one>
2024-04-11 20:42:23 +0300

    main general commit

--- src/main/java/edu/ntudp/fit/yakovlev/lab3/view/UniversityPrinter.java
+++ src/main/java/edu/ntudp/fit/yakovlev/lab3/view/UniversityPrinter.java
@@ -2,53 +2,45 @@ package edu.ntudp.fit.yakovlev.lab3.view;
 
 import edu.ntudp.fit.yakovlev.lab3.model.*;
 
-import java.util.List;
-
 public class UniversityPrinter {
-
     public void printUniversityTree(University university) {
         Head head = university.getHead();
-        System.out.println("└── " + university.getName()+ " University, Head: "+ head.getFirstName() + " " + head.getLastName());
+        System.out.printf("└── %s University, Head: %s %s%n",
+                university.getName(), head.getFirstName(), head.getLastName());
 
-        List<Faculty> faculties = university.getSubComponent();
-        for (int i = 0; i < faculties.size(); i++) {
-            Faculty faculty = faculties.get(i);
-            boolean isLast = (i == faculties.size() - 1);
-            printFacultyTree(faculty, (isLast ? "    " : "│   "));
+        for (Faculty faculty : university.getSubComponent()) {
+            printFacultyTree(faculty, "    ");
         }
     }
 
     public void printFacultyTree(Faculty faculty, String prefix) {
         Head head = faculty.getHead();
-        System.out.println(prefix + "└── " + faculty.getName() + " Faculty, Head: "+ head.getFirstName() + " " + head.getLastName());
+        System.out.printf(prefix + "└── %s Faculty, Head: %s %s%n",
+                faculty.getName(), head.getFirstName(), head.getLastName());
 
-        List<Department> departments = faculty.getSubComponent();
-        for (int i = 0; i < departments.size(); i++) {
-            Department department = departments.get(i);
-            boolean isLast = (i == departments.size() - 1);
-            printDepartmentTree(department, prefix + (isLast ? "    " : "│   "));
+        for (Department department : faculty.getSubComponent()) {
+            printDepartmentTree(department, prefix + "│   ");
         }
     }
 
     public void printDepartmentTree(Department department, String prefix) {
         Head head = department.getHead();
-        System.out.println(prefix + "└── " + department.getName() + " Department, Head: "+ head.getFirstName() + " " + head.getLastName());
+        System.out.printf(prefix + "└── %s Department, Head: %s %s%n",
+                department.getName(), head.getFirstName(), head.getLastName());
 
-        List<Group> groups = department.getSubComponent();
-        for (int i = 0; i < groups.size(); i++) {
-            Group group = groups.get(i);
-            boolean isLast = (i == groups.size() - 1);
-            printGroupTree(group, prefix + (isLast ? "    " : "│   "));
+        for (Group group : department.getSubComponent()) {
+            printGroupTree(group, prefix + "│   ");
         }
     }
 
     public void printGroupTree(Group group, String prefix) {
         Head head = group.getHead();
-        System.out.println(prefix + "└── " + group.getName() + " Group, Head: "+ head.getFirstName() + " " + head.getLastName());
+        System.out.printf(prefix + "└── %s Group, Head: %s %s%n",
+                group.getName(), head.getFirstName(), head.getLastName());
 
-        List<Student> students = group.getSubComponent();
-        for (Student student : students) {
-            System.out.println(prefix + "    └── " + student.getFirstName() + " " + student.getLastName() + " (Student)");
+        for (Student student : group.getSubComponent()) {
+            System.out.printf(prefix + "│    └── %s %s (Student)%n",
+                    student.getFirstName(), student.getLastName());
         }
     }
 }
